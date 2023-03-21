8e37436399fbf52244648cc6c243144e557d3216
Lucas Kahlil <kahlilpereira@gmail.com>
2023-03-20 21:22:08 -0300

    fix: import react

--- src/COMPONENTS/card-adicional/card-adicional.tsx
+++ src/COMPONENTS/card-adicional/card-adicional.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
 import './card-adicional.css'
