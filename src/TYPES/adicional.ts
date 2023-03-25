d250be95dc377a32bbefd5fa7c5401264074f440
Lucas Kahlil <kahlilpereira@gmail.com>
2023-03-24 21:38:29 -0300

    feat: add page product

--- src/TYPES/adicional.ts
+++ src/TYPES/adicional.ts
@@ -1,5 +1,5 @@
 export interface AdicionalPayload {
-    id?: string;
+    id: string;
     nome: string;
-    preco: string;
+    preco: number;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import { Category } from "./PAGES/category/category";
 import "./index.css";
 import ItensCategory from "./PAGES/itens-category/itens-category";
 import Order from "./PAGES/order/order";
-import App from "./PAGES/product-detail/product-detail";
 import Teste from "./COMPONENTS/table/teste";
 import GlobalContext from "./context";
+import ProductDetail from "./PAGES/product-detail/product-detail";
+import Side from "./COMPONENTS/sidebar/sidebar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -19,11 +20,12 @@ root.render(
       <Routes>
         <Route path="/categorys" element={<Category />} />
         <Route path="/itens" element={<ItensCategory />} />
-        <Route path="/product" element={<App />} />
         <Route path="/table" element={<Teste />} />
         <Route path="/categorys" element={<Category />} />
         <Route path="/itens-category" element={<ItensCategory />} />
         <Route path="/order" element={<Order />} />
+        <Route path="/product" element={<ProductDetail />} />
+        <Route path="/side" element={<Side />} />
       </Routes>
     </BrowserRouter>
     </GlobalContext>
