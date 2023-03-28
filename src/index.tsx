e84c2691581026b97c2541ee1ece2db53a6720c4
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-03-28 19:28:39 -0300

    fix: add rota home

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import ProductDetail from "./PAGES/product-detail/product-detail";
 import Side from "./COMPONENTS/sidebar/sidebar";
 import { Confirmation } from "./PAGES/confirmation/confirmation";
 import { Test4 } from "./PAGES/teste/teste";
+import Home from "./PAGES/home/home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -18,20 +19,21 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalContext>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/categorys" element={< Category />} />
-        <Route path="/itens" element={< ItensCategory />} />
-        <Route path="/table" element={< Teste />} />
-        <Route path="/categorys" element={< Category />} />
-        <Route path="/itens-category" element={< ItensCategory />} />
-        <Route path="/order" element={< Order />} />
-        <Route path="/product" element={< ProductDetail />} />
-        <Route path="/side" element={< Side />} />
-        <Route path="/confirmation" element={< Confirmation />} />
-        <Route path="/teste" element={< Test4 />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categorys" element={<Category />} />
+          <Route path="/itens" element={<ItensCategory />} />
+          <Route path="/table" element={<Teste />} />
+          <Route path="/categorys" element={<Category />} />
+          <Route path="/itens-category" element={<ItensCategory />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/product" element={<ProductDetail />} />
+          <Route path="/side" element={<Side />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/teste" element={<Test4 />} />
+        </Routes>
+      </BrowserRouter>
     </GlobalContext>
   </React.StrictMode>
 );
