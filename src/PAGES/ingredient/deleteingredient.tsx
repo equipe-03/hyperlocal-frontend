38ee31c864dad35627a65aec3c6f9594131a70ba
WendelOSantoss <wendelosantos@hotmail.com>
2023-03-28 19:38:20 -0300

    feat: requisição ingredients

---
+++ src/PAGES/ingredient/deleteingredient.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { IngredientPayload } from "../../TYPES/ingredient";
+import swal from "sweetalert";
+import { api } from "../../API/api";
+import React from "react";
+
+interface IngredientProps {
+  ingredients: IngredientPayload;
+  updatePage: () => void;
+}
+
+export function CardProfile({ ingredients, updatePage }: IngredientProps) {
+  const navigate = useNavigate();
+  async function DeleteCard() {
+    swal({
+      title: "Tem certeza que deseja deletar o ingrediente?",
+      icon: "warning",
+      dangerMode: true,
+      buttons: {
+        cancel: {
+          text: "Cancelar",
+          value: null,
+          visible: true,
+          closeModal: true,
+          className: "",
+        },
+        confirm: {
+          text: "Confirmar",
+          value: true,
+          visible: true,
+          closeModal: true,
+        },
+      },
+    }).then(async (res) => {
+      if (res) {
+        const isDeleted = await api.deleteIngredient(ingredients.id ?? "");
+        if (isDeleted) {
+          updatePage();
+        }
+      }
+    });
+  }
+}
--- src/TYPES/ingredient.ts
+++ src/TYPES/ingredient.ts
@@ -1,5 +1,5 @@
 export interface IngredientPayload {
-  id: string;
+  id?: string;
   name: string;
   status: string;
 }
