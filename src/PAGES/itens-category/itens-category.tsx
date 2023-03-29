a025782f4f0c02076e5f5742a000b10b119ed3e5
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-03-29 15:26:35 -0300

    fix: btn e rota

--- src/PAGES/itens-category/itens-category.tsx
+++ src/PAGES/itens-category/itens-category.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-empty-pattern */
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../API/api";
 import { CardItem } from "../../COMPONENTS/card-item/card-item";
 import { FooterHome } from "../../COMPONENTS/footer-home/footer-home";
@@ -8,41 +8,6 @@ import { ItensPayload } from "../../TYPES/tables";
 import "./itens-category.css";
 type Props = {};
 export default function ItensCategory({}: Props) {
-  //   const itens = [
-  //     {
-  //       id: "najdnaosdnapksda",
-  //       image:
-  //         "https://www.auau.com.br/image/cache/data/up_system/product-13697/TEXAS-BURGUER-1000x1000.jpg",
-  //       title: "X-Salada",
-  //       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //       preco: 20,
-  //     },
-  //     {
-  //       id: "najdnaosdnapksda32322",
-  //       image:
-  //         "https://www.auau.com.br/image/cache/data/up_system/product-13697/TEXAS-BURGUER-1000x1000.jpg",
-  //       title: "X-Salada",
-  //       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //       preco: 20,
-  //     },
-  //     {
-  //       id: "najdnaosdnapksda123213",
-  //       image:
-  //         "https://www.auau.com.br/image/cache/data/up_system/product-13697/TEXAS-BURGUER-1000x1000.jpg",
-  //       title: "X-Salada",
-  //       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //       preco: 20,
-  //     },
-  //     {
-  //       id: "najdnaosdnapksda9283821",
-  //       image:
-  //         "https://www.auau.com.br/image/cache/data/up_system/product-13697/TEXAS-BURGUER-1000x1000.jpg",
-  //       title: "X-Salada",
-  //       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  //       preco: 20,
-  //     },
-  //   ];
-
   const [itens, setItensList] = useState<ItensPayload[]>([]);
   useEffect(() => {
     async function loadItens() {
