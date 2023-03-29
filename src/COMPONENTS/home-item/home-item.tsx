56e3f7df5dcec3fbbbb157dcc64473a1243a28f9
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2023-03-28 22:35:31 -0300

    feature: implentacao itens

--- src/COMPONENTS/home-item/home-item.tsx
+++ src/COMPONENTS/home-item/home-item.tsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import "./home-item.css";
 type Props = {
   number: number;
 };
 
 export function HomeItem({ number }: Props) {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/categorys`);
+  };
   return (
     <div className="table">
-      <div className="table-item">
+      <div onClick={handleClick} className="table-item">
         <h2>{number}</h2>
       </div>
     </div>
--- src/TYPES/tables.d.ts
+++ src/TYPES/tables.d.ts
@@ -2,3 +2,11 @@ export type TablesPayload = {
   id?: string;
   number: number;
 };
+
+export type ItensPayload = {
+  id: string;
+  imgDish: string;
+  name: string;
+  description: string;
+  price: number;
+};
