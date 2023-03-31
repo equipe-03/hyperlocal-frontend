9fa3bbf7f1825a246ff0bedc7273b4c73b907945
WendelOSantoss <wendelosantos@hotmail.com>
2023-03-30 23:20:29 -0300

    feat: adicionando quantidade pratos footer

--- src/COMPONENTS/footer-product/footer-product.tsx
+++ src/COMPONENTS/footer-product/footer-product.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
-import './footer-product.css'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import "./footer-product.css";
+import { useNavigate } from "react-router-dom";
+import { DishPayload } from "../../TYPES/dish";
 
 export function FooterProduct() {
-  
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+  const [qtd, setQtd] = useState(0);
+
   return (
-    <div className='footer-product'>
-        <div className='quant-adc'>
-            <div>< FontAwesomeIcon icon={faMinus}/></div>
-            <div className='qtd'>0</div>
-            <div>< FontAwesomeIcon icon={faPlus}/></div>
-        </div>
-        <div className='btn-adc' onClick={() => navigate('/order')}>
-            <div>Adicionar</div>
-            <div>R$ 0,00</div>
-        </div>
+    <div className="footer-product">
+      <div className="quant-adc">
+        <button onClick={() => setQtd(Math.max(qtd - 1, 0))}>
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+        <div className="qtd">{qtd}</div>
+        <button onClick={() => setQtd(qtd + 1)}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+      <div className="btn-adc" onClick={() => navigate(`/order/`)}>
+        <div>Adicionar</div>
+        <div>R$ 12,00</div>
+      </div>
     </div>
-  )
+  );
 }
