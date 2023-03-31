5aa3d9e92288cba1ad72344c664177f9e9879ea4
João Victor Cavalcante <jvc881@gmail.com>
2023-03-31 17:20:00 -0300

    feat: Css crud final

--- src/COMPONENTS/lista-mesas/lista-mesa.tsx
+++ src/COMPONENTS/lista-mesas/lista-mesa.tsx
@@ -2,6 +2,7 @@ import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { TablePayload } from '../../TYPES/table'
 import React from 'react'
+import "./lista-mesa.css";
 
 interface ListaMesaProps {
     lista: TablePayload
@@ -9,13 +10,27 @@ interface ListaMesaProps {
 
 export function ListaMesa({ lista }: ListaMesaProps) {
     return (
-        <div>
-            <div className='container-name-buttons'>
-                <div className='nome-lista'>Mesa {lista.number}</div>
-                <div className='container-buttons'>
-                    <button> <FontAwesomeIcon icon={faTrash} /> </button>
-                </div>
-            </div>
+      <article className="card2">
+      <img
+        className="card__background"
+        src="https://images.unsplash.com/photo-1599458252573-56ae36120de1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVzdGF1cmFudCUyMHRhYmxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+        alt="Foto da Categoria"
+        width="1920"
+        height="2193"
+      />
+      <div className="card__content | flow">
+      <div className='lista-categorias'>
+              
+              <div className='container-buttons'>
+                  
+              </div>
+      </div>
+        <div className="card__content--container | flow">
+          <h2 className='card__title'>{lista.number}</h2>
+          
         </div>
+        <button className="noselect"><span className='text'>Deletar</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button>
+      </div>
+    </article>
     )
 }
--- src/COMPONENTS/lista-user/lista-user.tsx
+++ src/COMPONENTS/lista-user/lista-user.tsx
@@ -9,14 +9,27 @@ interface ListaUserProps {
 
 export function ListaUser({ lista }: ListaUserProps) {
     return (
-        <div>
-            <div className='container-name-buttons'>
-                <div className='nome-lista'>{lista.name}</div>
-                <div className='container-buttons'>
-                    <button> <FontAwesomeIcon icon={faPencil} /> </button>
-                    <button> <FontAwesomeIcon icon={faTrash} /> </button>
-                </div>
-            </div>
+        <article className="card2">
+       <img
+          className="card__background"
+          src="https://uploaddeimagens.com.br/images/004/413/017/full/Red_and_White.png?1680225517"
+          alt="Foto da Logo Chef Hub"
+          width="1920"
+          height="2193"
+        />
+    <div className="card__content | flow">
+    <div className="lista">
+      <div className="container-buttons">
+        <h2 className='card__title'>{lista.name}</h2>
+        <div className="container-buttons">
+        <div className="card__content--container | flow">
+        <button className="noselect"><span className='text'>Deletar</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button>
         </div>
+      </div>
+
+    </div>
+    </div>
+    </div>
+    </article>
     )
 }
